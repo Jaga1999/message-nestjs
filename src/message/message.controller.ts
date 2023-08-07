c56dcc0ac585cd9654652b67f500c1e81e10e29d
Jaga1999 <jagasiva1999@gmail.com>
2023-08-07 19:34:48 +0530

    Created Dummy CRUD route

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('message')
-export class MessageController {}
+export class MessageController {
+  @Get()
+  listMessages(): string {
+    return 'List Messages';
+  }
+
+  @Get('/:id')
+  getMessage(): string {
+    return 'Get Message';
+  }
+
+  @Post()
+  createMessage(): string {
+    return 'Create New Message';
+  }
+
+  @Put('/:id')
+  updateMessage(): string {
+    return 'Update Message';
+  }
+
+  @Delete('/:id')
+  deleteMessage(): string {
+    return 'Delete Message';
+  }
+}
